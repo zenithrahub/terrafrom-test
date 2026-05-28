4909dbbbcee346d9f0ba219e6cd2e8af74d5c9f9
Zenithra-Sonu_Kumar <sonukwebdev@gmail.com>
2026-05-29 02:11:04 +0530

    change autoscaling/main.tf

--- modules/autoscaling/main.tf
+++ modules/autoscaling/main.tf
@@ -1,3 +1,12 @@
+resource "aws_launch_template" "production_template" {
+
+  name_prefix = "${var.environment}-template"
+
+  image_id = "ami-03f4878755434977f"
+
+  instance_type = "t3.micro"
+}
+
 resource "aws_autoscaling_group" "production_asg" {
 
   name = "${var.environment}-asg"
@@ -11,4 +20,11 @@ resource "aws_autoscaling_group" "production_asg" {
   availability_zones = [
     "ap-south-1a"
   ]
+
+  launch_template {
+
+    id = aws_launch_template.production_template.id
+
+    version = "$Latest"
+  }
 }
