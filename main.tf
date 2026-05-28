6b82dff87ca87d874765b0fe53a44c347700af0f
Zenithra-Sonu_Kumar <sonukwebdev@gmail.com>
2026-05-28 21:20:28 +0530

    Changed S3 public setting to true

--- main.tf
+++ main.tf
@@ -9,8 +9,8 @@ resource "aws_s3_bucket" "public_bucket" {
 resource "aws_s3_bucket_public_access_block" "public_access" {
   bucket = aws_s3_bucket.public_bucket.id
 
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
