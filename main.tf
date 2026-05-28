d8f4ddb1819c0d9d212ea7d16104dc65e0cffbca
Zenithra-Sonu_Kumar <sonukwebdev@gmail.com>
2026-05-28 16:21:33 +0530

    Uploading the updated main.tf. So That Github action passes

--- main.tf
+++ main.tf
@@ -1,8 +1,11 @@
 resource "aws_instance" "web_server" {
   ami           = "ami-03f4878755434977f"
-  instance_type = "t3.micro"
+  instance_type = "t2.micro"
 
   tags = {
-    Name = "web-server"
+    Name        = "web-server"
+    Owner       = "platform-team"
+    Environment = "dev"
+    CostCenter  = "cloud-operations"
   }
 }
